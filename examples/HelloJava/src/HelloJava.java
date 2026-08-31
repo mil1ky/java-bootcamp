a215e155c007a2524004d7ee43b4f2b19a741838
Your Name <12345678+yourusername@users.noreply.github.com>
2026-08-31 17:25:01 -0400

    Exercise 1

--- examples/HelloJava/src/HelloJava.java
+++ examples/HelloJava/src/HelloJava.java
@@ -1,6 +1,9 @@
+import javax.tools.JavaCompiler;
+
 public class HelloJava {
-    public static void main(String[] args) {
-        System.out.println("Hello Java Bootcamp!");
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, JVM!");
     }
 }
 // Lab 0 startup

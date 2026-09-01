3e62f941f28916889620bbaa366e5b8abbcb535b
Your Name <12345678+yourusername@users.noreply.github.com>
2026-08-31 21:17:51 -0400

    Exercise 4

---
+++ examples/module-01-exercises/Helper.java
@@ -0,0 +1,7 @@
+public class Helper
+{
+    public static String tag()
+    {
+        return "helper-ok";
+    }
+}
---
+++ examples/module-01-exercises/LoadDemo.java
@@ -0,0 +1,8 @@
+public class LoadDemo
+{
+    public static void main(String[] args)
+    {
+        // TODO: print Helper.tag()
+        System.out.println(Helper.tag());
+    }
+}
